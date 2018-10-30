b398cd5f65b9e9321f93adee647bee6f46225adf
johnwayodi <wayodijohn@gmail.com>
2018-10-30 08:07:15 +0300

    [Feature #161575193] Add endpoints for user, product,sale,category

---
+++ storemanager/api/v2/models/schemas.py
@@ -0,0 +1,46 @@
+
+
+CATEGORY_SCHEMA = {
+    'type': 'object',
+    'maxProperties': 2,
+    'properties': {
+        'name': {'type': 'string'},
+        'description': {'type': 'string'},
+    },
+    'required': ['name', 'description']
+}
+
+PRODUCT_SCHEMA = {
+    'type': 'object',
+    'maxProperties': 6,
+    'properties': {
+        'name': {'type': 'string'},
+        'price': {'type': 'integer'},
+        'description': {'type': 'string'},
+        'category': {'type': 'string'},
+        'stock': {'type': 'integer'},
+        'min_stock': {'type': 'integer'},
+    },
+    'required': ['name', 'price', 'description', 'category', 'stock', 'min_stock']
+}
+
+USER_REGISTRATION_SCHEMA = {
+    'type': 'object',
+    'maxProperties': 3,
+    'properties': {
+        'username': {'type': 'string'},
+        'password': {'type': 'string'},
+        'role': {'type': 'string'}
+    },
+    'required': ['username', 'password', 'role']
+}
+
+USER_LOGIN_SCHEMA = {
+    'type': 'object',
+    'maxProperties': 2,
+    'properties': {
+        'username': {'type': 'string'},
+        'password': {'type': 'string'}
+    },
+    'required': ['username', 'password']
+}
